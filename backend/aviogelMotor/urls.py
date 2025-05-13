f4ff4e8c35bcda484ba8172e2be75d8ae1006d32
giac2610 <g.digiovanni@2610.it>
2025-05-13 17:18:28 +0200

    vabbe

--- backend/aviogelMotor/urls.py
+++ backend/aviogelMotor/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import update_config, move_motor, stop_motor, save_motor_config, get_motor_speeds
+from .views import update_config_view, move_motor_view, stop_motor_view, save_motor_config_view, get_motor_speeds_view
 
 urlpatterns = [
-    path('update/', update_config, name='update_config'),
-    path('move/', move_motor, name='move_motor'),
-    path('stop/', stop_motor, name='stop_motor'),
-    path('save/', save_motor_config, name='save_motor_config'),
-    path('speeds/', get_motor_speeds, name='get_motor_speeds'),
+    path('update/', update_config_view, name='update_config'),
+    path('move/', move_motor_view, name='move_motor'),
+    path('stop/', stop_motor_view, name='stop_motor'),
+    path('save/', save_motor_config_view, name='save_motor_config'),
+    path('speeds/', get_motor_speeds_view, name='get_motor_speeds'),
 ]
