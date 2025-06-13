c49491df635462b2997eb917a0b16427bef5d305
giac2610 <g.digiovanni@2610.it>
2025-06-13 15:23:50 +0200

    updatetest

--- src/app/pages/setup/setup.page.ts
+++ src/app/pages/setup/setup.page.ts
@@ -395,18 +395,4 @@ onPresetChange() {
     this.router.navigate(['/blob-simulation']);
   }
 
-
-  getMotorsRoute(){
-    this.configService.getMotorsRoute().subscribe({
-      next: (response) => {
-        this.presentToast('Percorso motore recuperato con successo', 'success');
-        console.log('Percorso motore:', response.route);
-        console.log('Coordinate motore:', response.motor_commands);
-      },
-      error: (error) => {
-        const errorMessage = error.error.detail || error.error.error || error.message;
-        this.presentToast(`Errore durante il recupero del percorso motore: ${errorMessage}`, 'danger');
-      }
-    });
-  }
 }
