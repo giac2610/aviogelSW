8a7e7ff6a7e08d79daba7623e89db136f30dfb18
giac2610 <g.digiovanni@2610.it>
2025-06-13 17:53:59 +0200

    refactoring

--- src/app/pages/blob-simulation/blob-simulation.page.ts
+++ src/app/pages/blob-simulation/blob-simulation.page.ts
@@ -12,19 +12,10 @@ import { SetupAPIService } from 'src/app/services/setup-api.service';
 export class BlobSimulationPage implements OnInit {
   keypoints: [number, number][] = [];
   coordinates: [number, number][] = [];
-  loading = false;
   error: string | null = null;
-  streamUrl: string = 'http://localhost:8000/camera/stream/?mode=fixed&keyframe=true';
-  graphUrl: string = 'http://localhost:8000/camera/plot_graph/';
-  homography: number[][] = [
-    [1,0,0],
-    [0,1,0],
-    [0,0,1]
-  ];
-  reconstructGridStatus: string | null = null;
-  boundingBoxVertices: [number, number][] = [];
-  parallelepipedVertices: [number, number][] = [];
-  dynamicWarpedStreamUrl: string;
+  streamUrl: string = '';
+  // graphUrl: string = 'http://localhost:8000/camera/plot_graph/';
+  graphUrl: string = '';
 
   @ViewChild('cameraImg', { static: false }) cameraImgRef!: ElementRef<HTMLImageElement>;
   imgWidth: number = 1280/3.5;
@@ -32,11 +23,9 @@ export class BlobSimulationPage implements OnInit {
 
   constructor(
     private toastController: ToastController,
-    private alertController: AlertController,
     private configService: SetupAPIService
   ) {
     this.streamUrl = this.configService.getNormalStreamUrl();
-    this.dynamicWarpedStreamUrl = this.configService.getDynamicWarpedStreamUrl();
   }
 
   ngOnInit() {
@@ -49,40 +38,9 @@ export class BlobSimulationPage implements OnInit {
         }
       },
     });
-    this.fetchKeypoints();
-    this.loadHomography();
     this.getRealCoordinates();
   }
 
-  fetchKeypoints() {
-    this.loading = true;
-    this.error = null;
-    this.configService.getKeypoints().subscribe({
-      next: (res) => {
-        this.keypoints = res.keypoints || [];
-        this.boundingBoxVertices = res.bounding_box_vertices || [];
-        this.parallelepipedVertices = res.parallelepiped_vertices || [];
-        this.loading = false;
-      },
-      error: (err) => {
-        this.error = 'Errore nel recupero dei keypoints';
-        this.loading = false;
-      }
-    });
-  }
-
-  loadHomography() {
-    this.configService.getSettings().subscribe({
-      next: (res) => {
-        console.log('Settings:', res);
-        if (res.camera.calibration.camera_matrix) {
-          this.homography = res.camera.calibration.camera_matrix;
-        } else {
-          this.presentToast('Nessuna omografia disponibile');
-        }
-      }
-    });
-  }
 
   async presentToast(message: string, color: string = 'success') {
     const toast = await this.toastController.create({
@@ -91,42 +49,6 @@ export class BlobSimulationPage implements OnInit {
       color: color
     });
     await toast.present();
-  }
-
-  onCameraLoad(img: HTMLImageElement) {
-    // Aggiorna solo se disponibili valori validi
-    if (img.naturalWidth && img.naturalHeight) {
-      this.imgWidth = img.naturalWidth;
-      this.imgHeight = img.naturalHeight;
-    }
-  }
-
-  calibrateCamera() {
-   this.configService.calibrateCamera().subscribe({
-      next: (res) => {
-        if (res.status === 'success') {
-          this.presentToast(res.message || 'Calibrazione completata con successo', 'success');
-          this.fetchKeypoints();
-        } else {
-          this.presentToast(res.message || 'Errore nella calibrazione', 'danger');
-        }
-      } 
-    })
-  }
-
-  saveFrameCalibration() {
-    this.configService.saveFrameCalibration().subscribe({
-      next: (res) => {
-        if (res.status === 'success') {
-          this.presentToast('Frame salvato: ' + res.filename);
-        } else {
-          this.presentToast('Errore nel salvataggio frame', 'danger');
-        }
-      },
-      error: () => {
-        this.presentToast('Errore nel salvataggio frame', 'danger');
-      }
-    });
   }
 
   getRealCoordinates(){
@@ -143,34 +65,24 @@ export class BlobSimulationPage implements OnInit {
     });
   }
 
-  setFixedPerspectiveView() {
-    this.configService.setFixedPerspectiveView().subscribe({
-      next: (res) => {
-        if (res.status === 'success') {
-          this.presentToast('Vista prospettica fissa impostata con successo');
-        } else {
-          this.presentToast(res.message || 'Errore nell\'impostazione della vista prospettica', 'danger');
-        }
-      },
-      error: (res) => {
-        this.presentToast(res.message || 'Errore nella richiesta di impostazione della vista prospettica', 'danger');
-      }
-    });
-  }
+executeRoute(){
+  this.configService.getMotorsRoute().subscribe({
+    next: (res) => {
+      if (res.status === 'success') {
+        this.presentToast('Rotta ottenuta con successo');
+        console.log('percorso:', res.motor_commands);
 
-  executeRoute(){
-    this.configService.getMotorsRoute().subscribe({
-      next: (res) => {
-        if (res.status === 'success') {
-          this.presentToast('Rotta ottenuta con successo');
-          console.log('percorso:', res.motor_commands);
-        } else {
-          this.presentToast(res.message || 'Errore nell\'esecuzione della rotta', 'danger');
+        // Se la risposta contiene l'immagine base64:
+        if (res.plot_graph_base64) {
+          this.graphUrl = 'data:image/png;base64,' + res.plot_graph_base64;
         }
-      },
-      error: (err) => {
-        this.presentToast('Errore nella richiesta della rotta', 'danger');
+      } else {
+        this.presentToast(res.message || 'Errore nell\'esecuzione della rotta', 'danger');
       }
-    });
-  }
+    },
+    error: (err) => {
+      this.presentToast('Errore nella richiesta della rotta', 'danger');
+    }
+  });
+}
 }
