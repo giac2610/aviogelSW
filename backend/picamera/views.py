de740e40f346c1d68895cab0ac2599f14cfb6dff
giac2610 <g.digiovanni@2610.it>
2025-07-30 12:48:33 +0200

    if not grid return false

--- backend/picamera/views.py
+++ backend/picamera/views.py
@@ -467,6 +467,8 @@ def _generate_grid_and_path(world_coords, camera_settings, velocita_x=4.0, veloc
 
     grid_analysis = check_grid_structure(world_coords)
     print(f"Grid analysis results: {grid_analysis}")
+    if grid_analysis.get('is_grid') is False:
+        return [], [], []
     grid_analysis_spacing_x = grid_analysis.get('mean_spacing_x', 50.0)
     grid_analysis_spacing_y = grid_analysis.get('mean_spacing_y', 50.0)
     if(grid_analysis_spacing_x < 40.0 or grid_analysis_spacing_y < 40.0 or grid_analysis_spacing_x > 55.0 or grid_analysis_spacing_y > 55.0):
