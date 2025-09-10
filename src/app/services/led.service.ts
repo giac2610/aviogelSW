e316bcb4776e46289a721c45d66cb71635929b36
giac2610 <g.digiovanni@2610.it>
2025-09-10 14:59:00 +0200

    refactor led service: ora fa direttamente le get da esp e non passa per django

--- src/app/services/led.service.ts
+++ src/app/services/led.service.ts
@@ -6,26 +6,27 @@ import { Injectable } from '@angular/core';
 })
 export class LedService {
   private baseUrl = 'http://localhost:8000/led/control-leds/';
+  private ESPurl = 'http://192.168.159.130:80/';
 
   constructor(private http: HttpClient) { }
 
   startWaveEffect() {
-    return this.http.post(this.baseUrl, { effect: 'wave' });
+    return this.http.get(this.ESPurl + 'rainbow');
   }
 
   startGreenLoading() {
-    return this.http.post(this.baseUrl, { effect: 'green_loading' });
+    return this.http.get(this.ESPurl + 'greenloading');
   }
 
   startYellowBlink() {
-    return this.http.post(this.baseUrl, { effect: 'yellow_blink' });
+    return this.http.get(this.ESPurl + 'yellowblink');
   }
 
   startRedStatic() {
-    return this.http.post(this.baseUrl, { effect: 'red_static' });
+    return this.http.get(this.ESPurl + 'redstatic');
   }
 
   stopEffect() {
-    return this.http.post(this.baseUrl, { effect: 'stop' });
+    return this.http.get(this.ESPurl + 'off');
   }
 }
