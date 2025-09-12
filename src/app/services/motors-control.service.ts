403d26bc1b97a19e126a2862ce4a07c3f9ffd318
giac2610 <g.digiovanni@2610.it>
2025-09-12 12:24:57 +0200

    daje co sti. led

--- src/app/services/motors-control.service.ts
+++ src/app/services/motors-control.service.ts
@@ -1,29 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-
+import { LedService } from './led.service';
 @Injectable({
   providedIn: 'root'
 })
 export class MotorsControlService {
-  private apiUrl = `http://${window.location.hostname}:8000/motors/` 
+  private apiUrl = `http://${window.location.hostname}:8000/motors/`
   
-  constructor(private http: HttpClient) { }
-  
-  updateSettings(body: any):Observable<any>{ 
+
+  constructor(
+    private http: HttpClient, 
+    private ledService: LedService
+  ) { }
+
+  updateSettings(body: any): Observable<any> {
     const url = `${this.apiUrl}update/`
     return this.http.post<any>(url, body);
-    }
+  }
 
-  moveMotor(body: any):Observable<any> {
+  moveMotor(body: any): Observable<any> {
     const url = `${this.apiUrl}move/`
     return this.http.post<any>(url, body);
   }
   // ferma tutti i motori
-  stopMotor():Observable<any>{
+  stopMotor(): Observable<any> {
     return this.http.post<any>(`${this.apiUrl}stop/`, null);
   }
-  saveSettings(body: any):Observable<any>{
+  saveSettings(body: any): Observable<any> {
     const url = `${this.apiUrl}save/`
     return this.http.post<any>(url, body);
   }
@@ -34,6 +38,10 @@ export class MotorsControlService {
 
   goHome(body: any): Observable<any> {
     const url = `${this.apiUrl}home/`;
+    this.ledService.startGreenLoading().subscribe({
+      next: () => console.log('Green loading avviato'),
+      error: () => console.error('Errore nell\'avviare il green loading')
+    });
     return this.http.post<any>(url, body);
   }
 
