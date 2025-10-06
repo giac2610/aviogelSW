3f11af7061bbbb6b835ba2d9035d949179899efc
giac2610 <g.digiovanni@2610.it>
2025-10-06 17:46:52 +0200

    ora funzia

--- backend/setupApp/urls.py
+++ backend/setupApp/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import get_settings, update_settings
+from .views import get_settings, update_settings, get_ip_address
 
 urlpatterns = [
     path('get/', get_settings, name='get_settings'),
     path('update/', update_settings, name='update_settings'),
-    path('get_ip/', get_settings, name='get_ip_address'),  # Aggiunta la nuova rotta per ottenere l'IP
+    path('get_ip/', get_ip_address, name='get_ip_address'),  # Aggiunta la nuova rotta per ottenere l'IP
 ]
