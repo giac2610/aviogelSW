3123fa58ada9b4d5fc8b4e05c7572d854bf0bb41
giac2610 <g.digiovanni@2610.it>
2025-10-10 17:47:05 +0200

    se non lo vedi adesso è grave

--- src/app/pages/setup/setup.page.ts
+++ src/app/pages/setup/setup.page.ts
@@ -117,11 +117,10 @@ export class SetupPage implements OnInit, OnDestroy, AfterViewInit {
                 { "0": "q", "1": "w", "2": "e", "3": "r", "4": "t", "5": "y", "6": "u", "7": "i", "8": "o", "9": "p"},
                 { "0": "a", "1": "s", "2": "d", "3": "f", "4": "g", "5": "h", "6": "j", "7": "k", "8": "l", "9": "à", "10": "ò", "11": "ù", "12": "è"},
                 { "0": "z", "1": "x", "2": "c", "3": "v", "4": "b", "5": "n", "6": "m", "7": ",", "8": ".", "9": "-"}
-            ]
+            ],
             });
         
         KioskBoard.run(nativeNumericInput, {
-        // Opzioni per la tastiera numerica
         keysArrayOfObjects: [
             { "0": "1", "1": "2", "2": "3" },
             { "0": "4", "1": "5", "2": "6" },
